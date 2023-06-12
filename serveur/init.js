a0fd2d9d5dd8cd182379fa4a22b765ebf6d184c7
Aymeric Surre <aymeric.surre@etu.univ-grenoble-alpes.fr>
2023-06-12 11:36:17 +0200

    -index.html, +init.js

---
+++ serveur/init.js
@@ -0,0 +1,64 @@
+const file = require('./students.json');
+
+const {
+  University,
+  Department,
+  AcademicYear,
+  Group,
+  Student
+} = require('./entities.js');
+
+const init = function () {
+  const iut2 = new University();
+
+  ['CJ', 'INFO', 'STID'].forEach((name, i) => {
+    const nb = [14, 23, 22, 9, 26, 24, 12, 22, 21, 20];
+    const department = new Department({
+      name: name
+    });
+
+    for (let k = 0; k < nb.length; k++) {
+      nb[k] = nb[k] + i;
+    }
+    for (let k = 0; k < nb.reduce((acc, curr) => acc + curr, 0); k++) {
+      const student = new Student(Object.values(file)[k]);
+      department.students.push(student);
+      student.department = department;
+    }
+    const academicYear = new AcademicYear({
+      label: '2022-2023'
+    });
+    department.years.push(academicYear);
+    academicYear.department = department;
+    let index = 0;
+    ['1a', '1b', '1c', '1d', '1e'].forEach((j, p) => {
+      const gr = new Group({
+        label: department.name.toLowerCase() + j
+      });
+      academicYear.groups.push(gr);
+      gr.year = academicYear;
+      for (let o = 0; o < nb[p]; o++) {
+        const st = department.students[index++];
+        gr.enrolled.push(st);
+        st.group = gr;
+      }
+    });
+    ['2a', '2b', '2c', '2d', '2e'].forEach((j, p) => {
+      const gr = new Group({
+        label: department.name.toLowerCase() + j
+      });
+      academicYear.groups.push(gr);
+      gr.year = academicYear;
+      for (let o = 0; o < nb[p + 5]; o++) {
+        const st = department.students[index++];
+        gr.enrolled.push(st);
+        st.group = gr;
+      }
+    });
+    iut2.departments.push(department);
+  });
+  return iut2;
+};
+
+module.exports = init;
+
