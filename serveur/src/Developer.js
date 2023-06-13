afa458e2e5c9d6be16b0ed6393a53bfd373cee73
Aymeric Surre <aymeric.surre@etu.univ-grenoble-alpes.fr>
2023-06-13 10:36:37 +0200

    Création de la class Developer

---
+++ serveur/src/Developer.js
@@ -0,0 +1,23 @@
+const data = require('./data/developers.json');
+
+class Developer {
+    constructor(data={}) {
+        this.id = data.id;
+        this.name = data.name;
+        this.games = [];
+    }
+
+    addGame(game) {
+        this.games.push(game);
+    }
+
+    getGames() {
+        return this.games;
+    }
+
+    getGameById(id) {
+        return this.games.find(game => game.id === id);
+    }
+
+}
+
